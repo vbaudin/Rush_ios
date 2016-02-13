9e24c1a0ed99599af42439984959d726bd225330
Vincent BAUDIN <vbaudin@e1r1p10.42.fr>
2016-02-13 14:31:42 +0100

    yolo

---
+++ FortyTwoMaps/SecondViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SecondViewController.swift
+//  FortyTwoMaps
+//
+//  Created by Vincent BAUDIN on 2/13/16.
+//  Copyright © 2016 Vincent BAUDIN. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.contentInset = UIEdgeInsetsMake(20.0, 0.0, 0.0, 0.0);
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section \(section)"
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) 
+        
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        
+        return cell
+    }
+}
+
